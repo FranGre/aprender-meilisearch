bd4e751bbae8bee7cc3167e2aedfdd02ae2f254f
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-05-13 23:09:16 +0200

    [MOD] - dark border in to Link

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -3,7 +3,7 @@ export default function Link({ text, href }: { text: string, href: string }) {
     return (
         <>
             <div className="my-2">
-                <a target="_blank" href={href} className="p-2 rounded bg-blue-400 hover:text-black hover:cursor-pointer hover:border-black hover:border">👉{text}</a>
+                <a target="_blank" href={href} className="p-2 rounded bg-blue-400 dark:text-black dark:hover:border-slate-100 hover:cursor-pointer hover:text-black  hover:border-black hover:border">👉{text}</a>
             </div>
         </>
     )
